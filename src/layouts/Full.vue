ec1e64dbd22be01078cfe5ee5e03ccdbd7cd71c9
RodrigoBonif <digoboni22@gmail.com>
2021-11-25 14:53:34 -0300

    first commit

---
+++ src/layouts/Full.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <nav class="menu">
+        <a href="/">Home</a>
+        <a href="/">Comunidades</a>
+        <a href="/">Comunidades</a>
+        <a href="/">Comunidades</a>
+        <a href="/">Comunidades</a>
+        <a href="/">Comunidades</a>
+        <a href="/">Comunidades</a>
+        <a href="/">Comunidades</a>
+
+        <a href="/about">Sobre</a>
+    </nav>
+    <div class="perfil">
+        <ion-icon @click="isDisplay = !isDisplay" class="icone-user" name="person-outline"></ion-icon>
+    </div>
+    <div class="box-perfil" :class="{'open-display': isDisplay}">
+        <h2 class="text-green">Perfil</h2>
+         <p class="text-white">/*Nome*/</p>  <!-- ou user, caso a pessoa não esteja logado  -->
+        <a href="/" class="link-perfil text-white">Gerenciar Perfil</a>
+    </div>
+
+    <main class="corpo">
+      <router-view/>
+    </main>
+
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDisplay: false
+    }
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="login">
+            <form class="comeco">
+                <div class="card-header">
+                    <h2>Login</h2>
+                </div>
+                <div class="conteudo">
+                    <div class="card-content-area">
+                        <label for="usuario">Usuário</label>
+                        <input type="text" id="usuario" autocomplete="off">
+                    </div>
+                    <div class="card-content-area">
+
+                        <label for="password">Senha</label>
+
+                        <input type="password" id="password" autocomplete="off">
+                    </div>
+                </div>
+                <div class="footer">
+
+                    <input type="submit" value="login" class="submit">
+
+                    <a href="#" class="recuperar_senha">Esqueceu a senha?</a>
+                </div>
+            </form>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
